3fee139a2124d7b26c6270aeb94096119d925ecc
tim9510019 <tim9510019@hotmail.com>
2016-08-17 19:05:37 +0800

    Support loading pictures in a folder
    Support loading pictures in a folder

--- Body_Hog/body.py
+++ Body_Hog/body.py
@@ -1,19 +1,36 @@
 import numpy as np
 import cv2
+import glob
 
-hog = cv2.HOGDescriptor()
+# Input Image from pic/ 
+# Find Pedestrian ROI and save to hogresult/
+# Calculate the hitrate
+# Ptype = "jpg", "bmp", ....
+def Hogdetect(Hit,Pic,Ptype):
+  for imgfile1 in glob.glob("pic/*."+ Ptype):
+    Pic +=1
+    img = cv2.imread(imgfile1)
 
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-img = cv2.imread('people.jpg')
-found,w=hog.detectMultiScale(img, winStride=(8,8), padding=(32,32), scale=1.05)
+    found,w=hog.detectMultiScale(img, winStride=(8,8), padding=(32,32), scale=1.05)
+  
+    if (len(found) > 0):
+      Hit +=1
+      for (x,y,w,h) in found:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
-for (x,y,w,h) in found:
-  cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-  #roi_gray = gray[y:y+h, x:x+w]
-  #roi_color = img[y:y+h, x:x+w]
-  #eyes = eye_cascade.detectMultiScale(roi_gray)
-  #for (ex,ey,ew,eh) in eyes:
-    #cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+      filename = imgfile1.split("pic/")[1]
+      cv2.imwrite("hogresult/" + filename.split("."+ Ptype)[0] + "_HOG." + Ptype, img)
+  return Hit, Pic
 
 
-cv2.imwrite('people.png', img)
+if __name__ == '__main__':
+
+  hog = cv2.HOGDescriptor()
+  hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+  Hit, Pic = 0, 0
+  Hit, Pic = Hogdetect(Hit,Pic,"jpg")
+  Hit, Pic = Hogdetect(Hit,Pic,"bmp")
+
+  hitrate = Hit / Pic
+  print("Hitrate = %.2f"% hitrate )
