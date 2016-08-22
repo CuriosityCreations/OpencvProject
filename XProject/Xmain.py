87bcc9af1daa7586208ced0647bcb7aa0c0d7913
tim9510019 <tim9510019@hotmail.com>
2016-08-22 17:26:03 +0800

    ROI AREA
    ROI AREA

--- XProject/Xmain.py
+++ XProject/Xmain.py
@@ -36,9 +36,13 @@ if __name__ == '__main__':
         thresh = cv2.dilate(imgRes, None, iterations=8)
         im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
+        number = 0;
         for cnt in contours:
           x,y,w,h = cv2.boundingRect(cnt)
           cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+          area = cv2.contourArea(cnt)
+          print("area = %4d, number = %d"%(area,number))
+          number+=1
        
         cv2.imshow('frame',frame)
         #cv2.imshow('res',imgRes)
