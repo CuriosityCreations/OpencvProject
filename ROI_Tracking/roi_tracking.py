bb758e6b3eba7c36aea7eed4eeb39d6775851a77
tim9510019 <tim9510019@hotmail.com>
2016-08-25 15:31:24 +0800

    Add ROI electing animation
    Add ROI electing animation

--- ROI_Tracking/roi_tracking.py
+++ ROI_Tracking/roi_tracking.py
@@ -8,27 +8,31 @@ refPt = []
 cropping = False
  
 def click_and_crop(event, x, y, flags, param):
-	# grab references to the global variables
-	global refPt, cropping
- 
-	# if the left mouse button was clicked, record the starting
-	# (x, y) coordinates and indicate that cropping is being
-	# performed
-	if event == cv2.EVENT_LBUTTONDOWN:
-		refPt = [(x, y)]
-		cropping = True
- 
-	# check to see if the left mouse button was released
-	elif event == cv2.EVENT_LBUTTONUP:
-		# record the ending (x, y) coordinates and indicate that
-		# the cropping operation is finished
-		refPt.append((x, y))
-		cropping = False
- 
-		# draw a rectangle around the region of interest
-		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
-		#cv2.imshow("image", image)
-                #cv2.imwrite('roi.jpg', image)
+    # grab references to the global variables
+    global refPt, cropping, ix,iy, image, clone
+
+    # if the left mouse button was clicked, record the starting
+    # (x, y) coordinates and indicate that cropping is being
+    # performed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        refPt = [(x, y)]
+        ix,iy = x,y
+        cropping = True
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if cropping == True:
+            image = clone.copy()
+            cv2.rectangle(image,(ix,iy),(x,y),[255, 0, 0],2)
+    # check to see if the left mouse button was released
+    elif event == cv2.EVENT_LBUTTONUP:
+        # record the ending (x, y) coordinates and indicate that
+        # the cropping operation is finished
+        refPt.append((x, y))
+        cropping = False
+
+        # draw a rectangle around the region of interest
+        cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
+        #cv2.imshow("image", image)
+        #cv2.imwrite('roi.jpg', image)
 
 # construct the argument parser and parse the arguments
 #ap = argparse.ArgumentParser()
@@ -36,9 +40,9 @@ def click_and_crop(event, x, y, flags, param):
 #args = vars(ap.parse_args())
  
 # load the image, clone it, and setup the mouse callback function
-camera = cv2.VideoCapture("(720p).mp4")
+camera = cv2.VideoCapture("inputcar.avi")
 ok, image=camera.read()
-image=cv2.resize(image,(0,0),fx = 0.25, fy = 0.25)
+#image=cv2.resize(image,(0,0),fx = 0.25, fy = 0.25)
 #image = cv2.imread("body2.jpg")
 clone = image.copy()
 cv2.namedWindow("image")
@@ -70,7 +74,7 @@ if len(refPt) == 2:
    bbox = (refPt[0][0],refPt[0][1],refPt[1][0]-refPt[0][0],refPt[1][1]-refPt[0][1])
    while camera.isOpened():
      ok, image=camera.read()
-     image=cv2.resize(image,(0,0),fx = 0.25, fy = 0.25)
+     #image=cv2.resize(image,(0,0),fx = 0.25, fy = 0.25)
      if not ok:
             print ('no image read')
             break
